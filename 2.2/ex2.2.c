66af8dab24e70691709d9211de743a5c561c96e3
SidneyJrLP1 <43341605+SidneyJrLP1@users.noreply.github.com>
2018-11-02 21:58:15 +0000

    Update ex2.2.c

--- 2.2/ex2.2.c
+++ 2.2/ex2.2.c
@@ -3,13 +3,21 @@ int main(void) {
   int n1;
   int n2;
   int i;
-  printf("Valor inicial: ");
+  int maior,menor;
+  printf("Valor 1: ");
   scanf("%d", &n1);
-  printf("Valor final: ");
+  printf("Valor 2: ");
   scanf("%d", &n2);
   printf("Valores entre %d e %d\n", n1, n2);
-  for(i = n1 + 1; i < n2; i++) {
+  if(n1 > n2) {
+    maior = n1;
+    menor = n2;
+  }else {
+    maior = n2;
+    menor = n1;
+  }
+  for(i = menor + 1; i < maior; i++) {
     printf("%d\n", i);
     }
   return 0;
-  }
+}
