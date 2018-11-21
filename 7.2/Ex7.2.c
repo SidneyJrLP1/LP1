a9eb813316b9c88db3e72d4e50149f0a3cb6a216
SidneyJrLP1 <43341605+SidneyJrLP1@users.noreply.github.com>
2018-11-21 10:26:17 -0200

    Update Ex7.2.c

--- 7.2/Ex7.2.c
+++ 7.2/Ex7.2.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
-struct Turma {
-   struct Aluno a1;
-   struct Aluno a2;
-   struct Aluno a3;
+
+struct Raca {
+   int vigor;
+   int def;
+   int atq;
 };
 
-struct Aluno {
-  float p1;
-  float p2;
-  float freq;
+struct Avatar {
+   char nome[256];
+   char classe[20];
+   struct Raca raca;
 };
 
-void preenche(struct Aluno* a) {
-  printf("Nota da P1: ");
-  scanf("%f", &((*a).p1));
-  printf("Nota da P2: ");
-  scanf("%f", &((*a).p2));
-  printf("Frequência: ");
-  scanf("%f", &((*a).freq));
-  if ((*a).freq > 1) {
-    printf("ERRO: NÚMERO INVÁLIDO\n");
-     exit (0);
-   }
+
+void preenche(struct Raca* A) {
+  char r[20];
+  printf("Raça: ");
+  scanf("%s", r);
+  printf("Vigor: ");
+  scanf("%d", &((*A).vigor));
+  printf("Defesa: ");
+  scanf("%d", &((*A).def));
+  printf("Ataque: ");
+  scanf("%d", &((*A).atq));
 }
-
-void preenche2(struct Turma* A) {
-  preenche(&(A.a1));
-  preenche(&(A.a2));
-  preenche(&(A.a3));
   
+  
+  
+void preenche2(struct Avatar* a) {
+  printf("Nome: ");
+  scanf("%s", ((*a).nome));
+  printf("Classe: ");
+  scanf("%s", ((*a).classe));
+  preenche(&(*a).raca);
+}  
+
 
 int main(void) {
-  struct Turma A;
-  preenche2(&A);
-  int i;
-  for (i = 1;i < 4; i++) { 
-     printf("Nota da P1: %.2f\n Nota da P2: %.2f\n Frequência: %.2f\n", ai.p1, ai.p2, ai.freq);
-    }
-  return 0;
+printf("CRIE SEU AVATAR\n");
+struct Avatar a1;
+preenche2(&a1);
+printf(" Nome: %s\n Classe: %s\n", a1.nome, a1.classe);
+printf("status da raça\n Vigor: %d\n Defesa: %d\n Ataque: %d\n", a1.raca.vigor, a1.raca.def, a1.raca.atq);
+return 0;
 }
