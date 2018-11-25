eb1cfa218fd455eb9bff0be886f4642d703912da
SidneyJrLP1 <43341605+SidneyJrLP1@users.noreply.github.com>
2018-11-25 22:33:52 +0000

    Create Ex7.5.c

---
+++ 7.5/Ex7.5.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+struct Mago {
+  int atq;
+  int def;
+  int vig;
+};
+
+struct Guerreiro {
+  int atq;
+  int def;
+  int vig;
+};
+
+struct Ladino {
+  int atq;
+  int def;
+  int vig;
+};
+
+union Classe {
+   struct Mago m;
+   struct Guerreiro g;
+   struct Ladino l;
+};
+
+struct Avatar {
+   char nome[256];
+   char raca[20];
+   union Classe X;
+};
+
+
+void preenche(union Classe* A, int I) {
+  printf("Escolha sua classe\n\n 1 = Mago\n 2 = Guerreiro\n 3 = Ladino");
+  if (I = 1) {
+    printf("Classe escolhida: Mago");
+  }else if(I = 2) {
+    printf("Classe escolhida: Guerreiro");
+  }else if(I = 3) {
+    printf("Classe escolhida: Ladino");
+  }
+}   
+void preenche2(struct Avatar* a) {
+  printf("Nome: ");
+  scanf("%s", ((*a).nome));
+  printf("Raca: ");
+  scanf("%s", ((*a).raca));
+}  
+
+
+int main(void) {
+printf("CRIE SEU AVATAR\n");
+struct Avatar a1;
+preenche2(&a1);
+int i;
+scanf("%d", &i);
+preenche(&(a1).X, i);
+printf(" Nome: %s\n Raca: %s\n", a1.nome, a1.raca);
+if (i = 1) {
+   printf("status da raça\n Vigor: %d\n Defesa: %d\n Ataque: %d\n", a1.X.m.vig, a1.X.m.def, a1.X.m.atq);
+}else if (i = 2) {
+   printf("status da raça\n Vigor: %d\n Defesa: %d\n Ataque: %d\n", a1.X.g.vig, a1.X.g.def, a1.X.g.atq);
+}else if (i = 3) {
+   printf("status da raça\n Vigor: %d\n Defesa: %d\n Ataque: %d\n", a1.X.l.vig, a1.X.l.def, a1.X.l.atq);
+}
+return 0;
+}
