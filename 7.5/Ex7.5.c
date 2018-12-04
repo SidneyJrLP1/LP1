9af4811a17ef4252b585be744b34390cfc060f1e
SidneyJrLP1 <43341605+SidneyJrLP1@users.noreply.github.com>
2018-12-04 18:09:47 +0000

    Update Ex7.5.c

--- 7.5/Ex7.5.c
+++ 7.5/Ex7.5.c
@@ -24,20 +24,19 @@ union Classe {
 };
 
 struct Avatar {
-   char nome[256];
+   char nome[20];
    char raca[20];
    union Classe X;
 };
 
 
 void preenche(union Classe* A, int I) {
-  printf("Escolha sua classe\n\n 1 = Mago\n 2 = Guerreiro\n 3 = Ladino");
-  if (I = 1) {
-    printf("Classe escolhida: Mago");
-  }else if(I = 2) {
-    printf("Classe escolhida: Guerreiro");
-  }else if(I = 3) {
-    printf("Classe escolhida: Ladino");
+  if (I == 1) {
+    printf("Classe escolhida: Mago\n");
+  }else if(I == 2) {
+    printf("Classe escolhida: Guerreiro\n");
+  }else if(I == 3) {
+    printf("Classe escolhida: Ladino\n");
   }
 }   
 void preenche2(struct Avatar* a) {
@@ -52,16 +51,29 @@ int main(void) {
 printf("CRIE SEU AVATAR\n");
 struct Avatar a1;
 preenche2(&a1);
+printf("ESCOLHA SUA CLASSE\n 1 = Mago\n 2 = Guerreiro\n 3 = Ladino\n");
 int i;
 scanf("%d", &i);
 preenche(&(a1).X, i);
 printf(" Nome: %s\n Raca: %s\n", a1.nome, a1.raca);
-if (i = 1) {
-   printf("status da raça\n Vigor: %d\n Defesa: %d\n Ataque: %d\n", a1.X.m.vig, a1.X.m.def, a1.X.m.atq);
-}else if (i = 2) {
-   printf("status da raça\n Vigor: %d\n Defesa: %d\n Ataque: %d\n", a1.X.g.vig, a1.X.g.def, a1.X.g.atq);
-}else if (i = 3) {
-   printf("status da raça\n Vigor: %d\n Defesa: %d\n Ataque: %d\n", a1.X.l.vig, a1.X.l.def, a1.X.l.atq);
+if (i == 1) { 
+   printf(" Classe: Mago\n");
+   a1.X.m.atq = 50;
+   a1.X.m.def = 40;
+   a1.X.m.vig = 20;
+   printf("--STATUS DO PERSONAGEM--\n Ataque: %d\n Defesa: %d\n Vigor: %d\n", a1.X.m.atq, a1.X.m.def, a1.X.m.vig);
+}else if (i == 2) {
+   printf(" Classe: Guerreiro\n");
+   a1.X.m.atq = 40;
+   a1.X.m.def = 60;
+   a1.X.m.vig = 30;
+   printf("--STATUS DO PERSONAGEM--\n Ataque: %d\n Defesa: %d\n Vigor: %d\n", a1.X.g.atq, a1.X.g.def, a1.X.g.vig);
+}else if (i == 3) {
+   printf(" Classe: Ladino\n");
+   a1.X.m.atq = 30;
+   a1.X.m.def = 20;
+   a1.X.m.vig = 50;
+   printf("--STATUS DO PERSONAGEM--\n Ataque: %d\n Defesa: %d\n Vigor: %d\n", a1.X.l.atq, a1.X.l.def, a1.X.l.vig);
 }
 return 0;
 }
