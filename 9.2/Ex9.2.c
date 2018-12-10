75f05536c98e8f3587526094595ef6748724bfbd
SidneyJrLP1 <43341605+SidneyJrLP1@users.noreply.github.com>
2018-12-10 10:41:59 -0200

    Update Ex9.2.c

--- 9.2/Ex9.2.c
+++ 9.2/Ex9.2.c
@@ -2,22 +2,24 @@
 struct Caixa {
    int valor;
    struct Caixa* prox;
+  };
 int main(void) {
    struct Caixa c1;
    struct Caixa c2;
    struct Caixa c3;
    struct Caixa c4;
    struct Caixa c5;
-   scanf("%d", c1.valor);
+   scanf("%d", &(c1.valor));
    c1.prox = &c2;	
-   scanf("%d", c2.valor);
+   scanf("%d", &(c2.valor));
    c2.prox = &c3;
-   scanf("%d", c3.valor);
+   scanf("%d", &(c3.valor));
    c3.prox = &c4;
-   scanf("%d", c4.valor);
+   scanf("%d", &(c4.valor));
    c4.prox = &c5;			
-   scanf("%d", c5.valor);
+   scanf("%d", &(c5.valor));
    c5.prox = NULL;
-   struct Caixa* lista = &c1
+   struct Caixa* lista = &c1;
    printf("%d->%d->%d->%d->%d", lista->valor, lista->prox->valor, lista->prox->prox->valor, lista->prox->prox->prox->valor, lista->prox->prox->prox->prox->valor);
    return 0;
+}
